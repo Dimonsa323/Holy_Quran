3c5f03fec5913fd0c21bb1b40a7a029ce640eaf1
Dima Hubenia <dima96gubenya@gmail.com>
2023-08-22 11:48:47 +0300

    laying of external elements

---
+++ Holy_Quran/Extensions/Color+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extension.swift
+//  Holy_Quran
+//
+//  Created by Дима Губеня on 22.08.2023.
+//
+
+import Foundation
---
+++ Holy_Quran/Extensions/Font+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Font+Extension.swift
+//  Holy_Quran
+//
+//  Created by Дима Губеня on 22.08.2023.
+//
+
+import Foundation
